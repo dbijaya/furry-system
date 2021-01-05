5e6e910dbc5f3911ffb50b9c9c7b4b29687938e8
bijaythapa <bijay.iwacademy@gmail.com>
2021-01-05 10:45:42 +0545

    [UPDATE] files updated

--- leads/urls.py
+++ leads/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from .api import LeadViewSet
 
 router = routers.DefaultRouter()
-router.register('api/leads/', LeadViewSet, "leads")
+router.register('api/leads', LeadViewSet, "leads")
 
 urlpatterns = router.urls
